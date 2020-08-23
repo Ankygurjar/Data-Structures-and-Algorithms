5dcebf572f892448f91b49b1dfc731cd41e685b6
Ankygurjar <ankitchoudhary699.ac@gmail.com>
2020-08-23 14:23:30 -0400

    final kruskal

--- DS and Algo/Algorithms/kruskulAlgo.js
+++ DS and Algo/Algorithms/kruskulAlgo.js
@@ -1,5 +1,6 @@
 class Edge{
-  constructor(end, weight){
+  constructor(start, end, weight){
+    this.start = start
     this.end = end
     this.weight = weight
   }
@@ -8,11 +9,57 @@ class Edge{
 class Kruskul{
   constructor(){
     this.graph = new Map()
+    this.spanningTree = new Set()
   }
 
   Display(){
-    for(let [key, value] of this.graph){
-      console.log(key, value)
+    console.log(this.spanningTree)
+  }
+
+  SetForNodes(){
+    let sets = []
+    let i = 0
+    for(let key of this.graph.keys()){
+      sets[i] = new Set([key])
+      i++
+    }
+    return sets
+  }
+
+  FindNodePresence(set, start, end){
+    for(let i = 0; i<set.length; i++){
+      if(set[i].has(start) && set[i].has(end)){
+        return false
+      }
+    }
+  }
+
+  FindNodeIndex(set, index){
+    for(let i = 0; i<set.length; i++){
+      if(set[i].has(index)){
+        return i
+      }
+    }
+  }
+
+  AddSetToTree(){
+    let sets = this.SetForNodes()
+    
+    let sortedEdges = this.SortingOfEdges()
+    //console.log(sortedEdges)
+    for(let i = 0; i<sortedEdges.length; i++){
+      if(!this.FindNodePresence(sets, sortedEdges[i].start, sortedEdges[i].end))
+        {
+          let indexOfStart = this.FindNodeIndex(sets, sortedEdges[i].start)
+          let indexOfEnd = this.FindNodeIndex(sets, sortedEdges[i].end)
+          let [values] = sets[indexOfEnd].values()
+          console.log(values)
+          sets[indexOfStart].add(values)
+          sets.splice(indexOfEnd, 1)
+          console.log(sets)
+          
+          
+        }
     }
   }
 
@@ -22,10 +69,38 @@ class Kruskul{
     }
   }
 
+  SortingOfEdges(){
+    let allEdges = []
+    let min = 0
+    let newArr = []
+    for(let edge of this.graph.values()){
+      allEdges.push(edge)
+    }
+
+    for(let edges of allEdges){
+      for(let i of edges){
+        newArr.push(i)
+      }
+    }
+    
+    for(let i = 0; i<newArr.length-1 ; i++){
+      min = i
+      for(let j = i; j<newArr.length; j++){
+        if(newArr[j].weight < newArr[min].weight){
+          min = j
+        }
+      }
+      let temp = newArr[min]
+      newArr[min] = newArr[i]
+      newArr[i] = temp
+    }
+    return newArr
+  }
+
   AddEdgesToNodes(start, end, weight){
     if(this.graph.has(start) && this.graph.has(end)){
       let arrOfChilds = this.graph.get(start)
-      arrOfChilds.push(new Edge(end, weight))
+      arrOfChilds.push(new Edge(start, end, weight))
     }
   }
 
@@ -45,19 +120,14 @@ ob.Main()
     ob.AddEdgesToNodes('v1', 'v4', 1)
     ob.AddEdgesToNodes('v1', 'v3', 4)
     ob.AddEdgesToNodes('v1', 'v2', 3)
-    ob.AddEdgesToNodes('v2', 'v1', 3)
     ob.AddEdgesToNodes('v2', 'v3', 2)
     ob.AddEdgesToNodes('v2', 'v5', 6)
-    ob.AddEdgesToNodes('v3', 'v1', 4)
-    ob.AddEdgesToNodes('v3', 'v2', 2)
     ob.AddEdgesToNodes('v3', 'v5', 2)
     ob.AddEdgesToNodes('v3', 'v6', 3)
-    ob.AddEdgesToNodes('v4', 'v1', 1)
     ob.AddEdgesToNodes('v4', 'v6', 5)
-    ob.AddEdgesToNodes('v5', 'v2', 6)
-    ob.AddEdgesToNodes('v5', 'v3', 2)
     ob.AddEdgesToNodes('v5', 'v6', 1)
-    ob.AddEdgesToNodes('v6', 'v3', 3)
-    ob.AddEdgesToNodes('v6', 'v4', 5)
-    ob.AddEdgesToNodes('v6', 'v5', 1)
-ob.Display()
+//ob.Display()
+ob.SetForNodes()
+ob.SortingOfEdges()
+ob.AddSetToTree()
+//ob.Display()
