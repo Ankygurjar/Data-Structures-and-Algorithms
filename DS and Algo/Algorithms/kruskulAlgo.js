5f6dfc69d0647608507d5efaaeb9c84104dc1217
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2020-08-23 09:58:42 -0400

    kruskul

--- DS and Algo/Algorithms/kruskulAlgo.js
+++ DS and Algo/Algorithms/kruskulAlgo.js
@@ -1,6 +1,15 @@
+class Edge{
+  constructor(first, second, weight){
+    this.first = first
+    this.second = second
+    this.weight = weight
+  }
+}
+
 class KrushKal{
     constructor(){
         this.graph = new Map()
+
     }
 
     AddNodesToGraph(node){
@@ -22,20 +31,38 @@ class KrushKal{
         }
     }
 
-    AddEdgesToNodes(node, edges){
-        let {graph} = this
-        if(graph.has(node)){
-            for(let edge of edges){
-                let childsOfNode = graph.get(node)
-                childsOfNode.push(edge)
-            }
+    AddEdgesToNodes(node, edge){
+      let {graph} = this
+      if(graph.has(node)){
+
         }
+      }
     }
 
-    Graph(nodes){
+    Graph(){
+      let {AddNodeToTree, AddEdgesToNodes, Display} = this
         for(let node of nodes){
-            this.AddNodesToGraph(node)
+            AddNodesToGraph(node)
         }
+        AddEdgesToNodes('v1', 'v4', 1)
+        AddEdgesToNodes('v1', 'v3', 4)
+        AddEdgesToNodes('v1', 'v2', 3)
+        AddEdgesToNodes('v2', 'v1', 3)
+        AddEdgesToNodes('v2', 'v3', 2)
+        AddEdgesToNodes('v2', 'v5', 6)
+        AddEdgesToNodes('v3', 'v1', 4)
+        AddEdgesToNodes('v3', 'v2', 2)
+        AddEdgesToNodes('v3', 'v5', 2)
+        AddEdgesToNodes('v3', 'v6', 3)
+        AddEdgesToNodes('v4', 'v1', 1)
+        AddEdgesToNodes('v4', 'v6', 5)
+        AddEdgesToNodes('v5', 'v2', 6)
+        AddEdgesToNodes('v5', 'v3', 2)
+        AddEdgesToNodes('v5', 'v6', 1)
+        AddEdgesToNodes('v6', 'v3', 3)
+        AddEdgesToNodes('v6', 'v4', 5)
+        AddEdgesToNodes('v6', 'v5', 1)
+        Display()
     }
 
 }
@@ -47,12 +74,4 @@ const nodes = new Array('v1', 'v2', 'v3', 'v4', 'v5', 'v6')
 
 ob.Graph(nodes)
 
-ob.AddEdgesToNodes('v1', ['v2','v3', 'v4'])
-ob.AddEdgesToNodes('v2', ['v1','v3', 'v5'])
-ob.AddEdgesToNodes('v3', ['v1','v2', 'v5','v6'])
-ob.AddEdgesToNodes('v4', ['v1','v6'])
-ob.AddEdgesToNodes('v5', ['v2','v3', 'v6'])
-ob.AddEdgesToNodes('v6', ['v5','v3', 'v6'])
-
-ob.Display()
-ob.AddNodeToTree()
+//ob.Display()
