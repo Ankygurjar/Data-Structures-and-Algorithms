b34b160d48efa93439c802d4a03ecd908ade260b
Ankygurjar <ankitchoudhary699.ac@gmail.com>
2020-08-23 10:44:38 -0400

    kruskul update

--- DS and Algo/Algorithms/kruskulAlgo.js
+++ DS and Algo/Algorithms/kruskulAlgo.js
@@ -1,77 +1,63 @@
 class Edge{
-  constructor(first, second, weight){
-    this.first = first
-    this.second = second
+  constructor(end, weight){
+    this.end = end
     this.weight = weight
   }
 }
 
-class KrushKal{
-    constructor(){
-        this.graph = new Map()
+class Kruskul{
+  constructor(){
+    this.graph = new Map()
+  }
 
+  Display(){
+    for(let [key, value] of this.graph){
+      console.log(key, value)
+    }
+  }
+
+  AddNodeToGraph(node){
+    if(!this.graph.has(node)){
+      this.graph.set(node, [])
+    }
+  }
+
+  AddEdgesToNodes(start, end, weight){
+    if(this.graph.has(start) && this.graph.has(end)){
+      let arrOfChilds = this.graph.get(start)
+      arrOfChilds.push(new Edge(end, weight))
+    }
+  }
+
+  Main(){
+    const nodeOfGraph = ['v1', 'v2', 'v3', 'v4', 'v5', 'v6']
+    for(let node of nodeOfGraph){
+      this.AddNodeToGraph(node)
     }
 
-    AddNodesToGraph(node){
-        if(!this.graph.has(node)){
-            this.graph.set(node, [])
-        }
-    }
 
-    Display(){
-        for(let [key, value] of this.graph){
-            console.log(key, value)
-        }
-    }
-
-    AddNodeToTree(){
-        let arrOfNodes = {}
-        for(let key of this.graph.keys()){
-            arrOfNodes[key] = new Set()
-        }
-    }
-
-    AddEdgesToNodes(node, edge){
-      let {graph} = this
-      if(graph.has(node)){
-
-        }
-      }
-    }
-
-    Graph(){
-      let {AddNodeToTree, AddEdgesToNodes, Display} = this
-        for(let node of nodes){
-            AddNodesToGraph(node)
-        }
-        AddEdgesToNodes('v1', 'v4', 1)
-        AddEdgesToNodes('v1', 'v3', 4)
-        AddEdgesToNodes('v1', 'v2', 3)
-        AddEdgesToNodes('v2', 'v1', 3)
-        AddEdgesToNodes('v2', 'v3', 2)
-        AddEdgesToNodes('v2', 'v5', 6)
-        AddEdgesToNodes('v3', 'v1', 4)
-        AddEdgesToNodes('v3', 'v2', 2)
-        AddEdgesToNodes('v3', 'v5', 2)
-        AddEdgesToNodes('v3', 'v6', 3)
-        AddEdgesToNodes('v4', 'v1', 1)
-        AddEdgesToNodes('v4', 'v6', 5)
-        AddEdgesToNodes('v5', 'v2', 6)
-        AddEdgesToNodes('v5', 'v3', 2)
-        AddEdgesToNodes('v5', 'v6', 1)
-        AddEdgesToNodes('v6', 'v3', 3)
-        AddEdgesToNodes('v6', 'v4', 5)
-        AddEdgesToNodes('v6', 'v5', 1)
-        Display()
-    }
+  }
 
 }
 
-
-const ob = new KrushKal()
-
-const nodes = new Array('v1', 'v2', 'v3', 'v4', 'v5', 'v6')
-
-ob.Graph(nodes)
-
-//ob.Display()
+const ob = new Kruskul()
+ob.Main()
+    ob.AddEdgesToNodes('v1', 'v4', 1)
+    ob.AddEdgesToNodes('v1', 'v3', 4)
+    ob.AddEdgesToNodes('v1', 'v2', 3)
+    ob.AddEdgesToNodes('v2', 'v1', 3)
+    ob.AddEdgesToNodes('v2', 'v3', 2)
+    ob.AddEdgesToNodes('v2', 'v5', 6)
+    ob.AddEdgesToNodes('v3', 'v1', 4)
+    ob.AddEdgesToNodes('v3', 'v2', 2)
+    ob.AddEdgesToNodes('v3', 'v5', 2)
+    ob.AddEdgesToNodes('v3', 'v6', 3)
+    ob.AddEdgesToNodes('v4', 'v1', 1)
+    ob.AddEdgesToNodes('v4', 'v6', 5)
+    ob.AddEdgesToNodes('v5', 'v2', 6)
+    ob.AddEdgesToNodes('v5', 'v3', 2)
+    ob.AddEdgesToNodes('v5', 'v6', 1)
+    ob.AddEdgesToNodes('v6', 'v3', 3)
+    ob.AddEdgesToNodes('v6', 'v4', 5)
+    ob.AddEdgesToNodes('v6', 'v5', 1)
+ob.Display()
